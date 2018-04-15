237c361b0f4417c94e9e6edda695d2518a9a0391
Anton Felix Lorenzen <anfelor@posteo.de>
2018-04-15 16:59:32 +0200

    Check all files in travis.ci script

---
+++ index.sh
@@ -0,0 +1,5 @@
+rm -rf index.agda;
+echo "module index where" >> index.agda;
+for i in $( find src -name "*.agda" | sed 's/src\/\(.*\)\.agda/\1/' | sed 's/\//\./g' | sort ); do
+    echo "import $i" >> index.agda;
+done
